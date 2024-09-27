d221908f0316177c057c3cf93a1e9828aee80f88
Josh Kleckner <j.kleckner15@gmail.com>
2024-09-27 16:30:23 -0400

    Josh Kleckner Project 7

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-541aa81b-c8ce-40c4-b6f8-e9d9be1d96fc.containerhub.tripleten-services.com"
 }
--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Response status code should 200', async () => {
+	let actualStatus;
 	try {
 		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		actualStatus = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatus).toBe(200);
 });
